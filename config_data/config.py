60daab360c1a465253e9f326cb94b34fe18c5da2
Alexey Dvvin <alexey.dubrovvin.gh@gmail.com>
2024-12-06 02:41:39 +0600

    bot done

--- config_data/config.py
+++ config_data/config.py
@@ -1,20 +1,18 @@
 from dataclasses import dataclass
-
-import environs
 from environs import Env
 
 
 @dataclass
 class TgBot:
-    token: str
+    token: str  # Токен для доступа к телеграм-боту
+
 
 @dataclass
 class Config:
-    bot: TgBot
+    tg_bot: TgBot
 
 
 def load_config(path: str | None = None) -> Config:
     env = Env()
-    env.read_env(path=path)
-    return Config(TgBot(token=env('BOT_TOKEN')))
-
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
